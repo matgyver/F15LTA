0c71007254864189e81c0bd2e9106b2d3896bdff
Matthew Nelson <kb0mgq@gmail.com>
2015-12-01 09:45:36 -0600

    Added student skeleton code
    Added student code to start from.

---
+++ Arduino/LTA_Example/LTA_Example.ino
@@ -0,0 +1,31 @@
+/*
+ * AerE 160 LTA Example Code
+ * Matthew E. Nelson
+ */
+
+
+// Includes
+#include <IRremote.h>
+
+// Defines
+
+IRsend irsend;
+
+#define IR_ONE 0x1CE3A857
+#define IR_TWO 0x1CE6897
+#define IR_THREE 0x1CE3E817
+#define IR_FOUR 0x1CE39867
+#define IR_FIVE 0x1CE358A7
+#define IR_SIX 0x1CE3D827
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // Send a "1" to fix Window 1
+  irsend.sendNEC(IR_ONE,32);
+  
+
+}

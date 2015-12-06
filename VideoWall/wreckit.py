bc195519ee0da67b1b7e2fa4c9fd221138d29da1
Matthew Nelson <kb0mgq@gmail.com>
2015-12-05 23:12:29 -0600

    New images
    Updated images, fixed in code.

--- VideoWall/wreckit.py
+++ VideoWall/wreckit.py
@@ -13,7 +13,8 @@ FPS = 30 # frames per second setting
 fpsClock = pygame.time.Clock()
 
 # set up the window
-DISPLAYSURF = pygame.display.set_mode((1280, 1080), 0, 32)
+DISPLAYSURF = pygame.display.set_mode(modes[0],FULLSCREEN,32)
+#DISPLAYSURF = pygame.display.set_mode((1280, 1080), 0, 32)
 pygame.display.set_caption('Animation')
 
 WHITE = (255, 255, 255)
@@ -28,7 +29,7 @@ while True: # the main game loop
     #If the button is pressed break the window
     if (GPIO.input(17) == True):
         winImg=pygame.image.load('g66841.png')
-    
+
     DISPLAYSURF.fill(BLACK)
     DISPLAYSURF.blit(winImg, (winx, winy))
 
@@ -39,15 +40,15 @@ while True: # the main game loop
     codeIR = lirc.nextcode()
     if codeIR:
         if codeIR[0] == 'fix':
-            winImg=pygame.image.load('window2.png')
+            winImg=pygame.image.load('window_LTA.png')
         elif codeIR[0] == 'break':
-            winImg=pygame.image.load('g66841.png')
+            winImg=pygame.image.load('windowbroken_LTA.png')
 
     if event.type == pygame.KEYDOWN:
         if (event.key == K_1):
-            winImg=pygame.image.load('window.png')
+            winImg=pygame.image.load('window_LTA.png')
         elif (event.key == K_2):
-            winImg=pygame.image.load('broken.png')
+            winImg=pygame.image.load('windowbroken_LTA.png')
 
     DISPLAYSURF.blit(winImg, (winx, winy))
 

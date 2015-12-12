fc6253894fac2415020778781be7fbec9556272e
Matthew Nelson <kb0mgq@gmail.com>
2015-12-12 00:08:50 -0600

    Updated software, release version used
    Version used in the competition

--- VideoWall/wreckit.py
+++ VideoWall/wreckit.py
@@ -11,27 +11,69 @@ GPIO.setup(17, GPIO.IN)
 
 FPS = 30 # frames per second setting
 fpsClock = pygame.time.Clock()
+screen = pygame.display.get_surface()
 
 # set up the window
-#DISPLAYSURF = pygame.display.set_mode(modes[0],FULLSCREEN,32)
-DISPLAYSURF = pygame.display.set_mode((1280, 1080), 0, 32)
-pygame.display.set_caption('Animation')
 
+screen = pygame.display.set_mode((0,0), FULLSCREEN, 32)
+#screen = pygame.display.set_mode((1080,768),0,32)
+pygame.display.set_caption('LTA 2015')
+w,h = screen.get_size()
+winx,winy = (10,10)
+
+#Setup the game screen and add splash screen
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
-winImg = pygame.image.load('window2.png')
-winx = 10
-winy = 10
-direction = 'right'
+
+winImg = pygame.image.load('insert_coin.jpg')
+winImg=pygame.transform.scale(winImg,(w, h))
+screen.fill(BLACK)
+screen.blit(winImg, (winx, winy))
+#Start the socket for the IR
 sockid = lirc.init("wreckit", blocking = False)
+
+#Determine what no. monitor we should be
+done = False
+pygame.display.update()
+while not done:
+    codeIR = lirc.nextcode()
+    if codeIR:
+        if codeIR[0] == 'ir_one':
+           monitor = 1
+           done = True
+        elif codeIR[0] == 'ir_two':
+            monitor = 2
+            done = True
+        elif codeIR[0] == 'ir_three':
+            monitor = 3
+            done = True
+        elif codeIR[0] == 'ir_four':
+            monitor = 4
+            done = True
+        elif codeIR[0] == 'ir_five':
+            monitor = 5
+            done = True
+        elif codeIR[0] == 'ir_six':
+            monitor = 6
+            done = True
+
+#Start the game
+winImg=pygame.image.load('window_LTA.png')
+winImg=pygame.transform.scale(winImg,(w, h))
+
+#Configure the interrupt for the GPIO pin
+GPIO.add_event_detect(17,GPIO.FALLING)
+
 while True: # the main game loop
 
     #If the button is pressed break the window
-    if (GPIO.input(17) == True):
+    #if (GPIO.input(17) == True):
+    if GPIO.event_detected(17):
         winImg=pygame.image.load('windowbroken_LTA.png')
+        winImg=pygame.transform.scale(winImg,(w, h))
 
-    DISPLAYSURF.fill(BLACK)
-    DISPLAYSURF.blit(winImg, (winx, winy))
+    screen.fill(BLACK)
+    screen.blit(winImg, (winx, winy))
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -41,16 +83,32 @@ while True: # the main game loop
     if codeIR:
         if codeIR[0] == 'ir_nine':
             winImg=pygame.image.load('window_LTA.png')
+            winImg=pygame.transform.scale(winImg,(w, h))
         elif codeIR[0] == 'ir_zero':
             winImg=pygame.image.load('windowbroken_LTA.png')
-
-    if event.type == pygame.KEYDOWN:
-        if (event.key == K_1):
+            winImg=pygame.transform.scale(winImg,(w, h))
+        elif codeIR[0] == 'ir_one' and (monitor==1):
             winImg=pygame.image.load('window_LTA.png')
-        elif (event.key == K_2):
-            winImg=pygame.image.load('windowbroken_LTA.png')
+            winImg=pygame.transform.scale(winImg,(w, h))
+        elif codeIR[0] == 'ir_two' and (monitor==2):
+            winImg=pygame.image.load('window_LTA.png')
+            winImg=pygame.transform.scale(winImg,(w, h))
+        elif codeIR[0] == 'ir_three' and (monitor==3):
+            winImg=pygame.image.load('window_LTA.png')
+            winImg=pygame.transform.scale(winImg,(w, h))
+        elif codeIR[0] == 'ir_four' and (monitor==4):
+            winImg=pygame.image.load('window_LTA.png')
+            winImg=pygame.transform.scale(winImg,(w, h))
+        elif codeIR[0] == 'ir_five' and (monitor==5):
+            winImg=pygame.image.load('window_LTA.png')
+            winImg=pygame.transform.scale(winImg,(w, h))
+        elif codeIR[0] == 'ir_six' and (monitor==6):
+            winImg=pygame.image.load('window_LTA.png')
+            winImg=pygame.transform.scale(winImg,(w, h))
+        elif codeIR[0] == 'ir_pwr':
+            pygame.quit()
+            sys.exit()
 
-    DISPLAYSURF.blit(winImg, (winx, winy))
-
+    screen.blit(winImg, (winx, winy))
     pygame.display.update()
     fpsClock.tick(FPS)
